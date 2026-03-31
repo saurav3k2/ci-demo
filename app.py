a22c6bb13cf776c409c1cb75418d6a28ad8e1c5f
saurav3k2 <kr.sorab@gmail.com>
2026-03-31 16:13:12 +0530

    minor change

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
 
 
 
 
-def sub(a,b):return a-b
+def sub(a, b):
+    return a - b
 
-result1 = add(5,4)
-result2= sub(4,1)
+result1 = add(5, 4)
+result2 = sub(4, 1)
 
 
 print(result1, result2)
